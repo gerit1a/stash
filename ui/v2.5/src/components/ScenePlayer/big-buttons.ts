737f3f5f480a39d22d5ee9db0003e709af53882e
gerit1a <10052885+gerit1a@users.noreply.github.com>
2022-08-11 20:50:32 -0500

    Add double tap to seek

--- ui/v2.5/src/components/ScenePlayer/big-buttons.ts
+++ ui/v2.5/src/components/ScenePlayer/big-buttons.ts
@@ -22,23 +22,76 @@ class BigButtonGroup extends videojs.getComponent("Component") {
   constructor(player: VideoJsPlayer, options: any) {
     super(player, options);
 
-    this.addChild("seekButton", {
-      direction: "back",
-      seconds: 10,
-    });
+    this.addChild("seekButton", { direction: "back" }, 0);
 
-    this.addChild("BigPlayPauseButton");
+    this.addChild("BigPlayPauseButton", {}, 1);
 
-    this.addChild("seekButton", {
-      direction: "forward",
-      seconds: 10,
-    });
+    this.addChild("seekButton", { direction: "forward" }, 2);
   }
 
   createEl() {
     return super.createEl("div", {
       className: "vjs-big-button-group",
     });
+  }
+}
+
+class touchControls extends videojs.getPlugin("plugin") {
+  /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
+  timer: NodeJS.Timeout | null = null;
+  backIconEl: HTMLElement | null = null;
+  forwardIconEl: HTMLElement | null = null;
+
+  constructor(player: VideoJsPlayer, options: any) {
+    super(player, options);
+
+    const bigButtonGroup = player.getChild("BigButtonGroup");
+    if (!videojs.browser.TOUCH_ENABLED || bigButtonGroup == null) {
+      return;
+    }
+    this.backIconEl = bigButtonGroup.children()[0].el() as HTMLElement;
+    this.forwardIconEl = bigButtonGroup.children()[2].el() as HTMLElement;
+    player.on("touchstart", this.onTouchStart);
+  }
+
+  onTouchStart = (event: TouchEvent) => {
+    this.handleTouchStart(event);
+  };
+
+  handleTouchStart(event: TouchEvent) {
+    if (!this.player.hasStarted()) {
+      return;
+    }
+    const playerWidth = this.player.el().getBoundingClientRect().width;
+    const touchLocation = event.changedTouches[0]?.clientX;
+    const touchPercent = touchLocation / playerWidth;
+
+    if (this.timer == null) {
+      this.timer = setTimeout(() => {
+        this.timer = null;
+      }, 250);
+      return;
+    }
+    clearTimeout(this.timer);
+    this.timer = null;
+
+    if (touchPercent > 0.65) {
+      this.displayIcon(this.forwardIconEl);
+      this.player.currentTime(this.player.currentTime() + 5);
+    } else if (touchPercent < 0.35) {
+      this.displayIcon(this.backIconEl);
+      this.player.currentTime(Math.max(0, this.player.currentTime() - 5));
+    }
+  }
+
+  displayIcon(el: HTMLElement | null) {
+    if (el == null) {
+      return;
+    }
+    el.classList.add("visible");
+    setTimeout(() => {
+      el.classList.remove("visible");
+    }, 100);
   }
 }
 
@@ -50,5 +103,13 @@ const bigButtons = function (this: VideoJsPlayer) {
 videojs.registerComponent("BigButtonGroup", BigButtonGroup);
 videojs.registerComponent("BigPlayPauseButton", BigPlayPauseButton);
 videojs.registerPlugin("bigButtons", bigButtons);
+videojs.registerPlugin("touchControls", touchControls);
+
+declare module "video.js" {
+  export interface VideoJsPlayer {
+    bigButtons: () => typeof bigButtons;
+    touchControls: () => touchControls;
+  }
+}
 
 export default bigButtons;
