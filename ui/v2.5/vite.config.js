5b802db92ced58c24732741633b9a3c1a8757067
gerit1a <10052885+gerit1a@users.noreply.github.com>
2022-11-06 14:43:08 -0600

    Add pwa

--- ui/v2.5/vite.config.js
+++ ui/v2.5/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import tsconfigPaths from "vite-tsconfig-paths";
 import viteCompression from 'vite-plugin-compression';
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -19,9 +20,38 @@ export default defineConfig({
   plugins: [tsconfigPaths(),
     viteCompression({
     algorithm: 'gzip',
-    disable: false,
-    deleteOriginFile: true,
-    filter: /\.(js|json|css|svg|md)$/i
-  })
-],
+      disable: false,
+      deleteOriginFile: true,
+      filter: /\.(js|json|css|svg|md)$/i,
+    }),
+    VitePWA({
+      includeAssets: ["favicon.ico", "apple-touch-icon.png"],
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Stash: Porn Organizer",
+        short_name: "Stash",
+        display: "standalone",
+        background_color: "#FFFFFF",
+        theme_color: "#000000",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ],
+      },
+    }),
+  ],
 })
